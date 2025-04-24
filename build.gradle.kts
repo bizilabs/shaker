2c1b7b3702028a47cab13feb81804813307ce9c7
mambo <mambobryan@gmail.com>
2025-04-25 01:22:14 +0300

    :rocket: updates workflow for pushing to main

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,7 +41,7 @@ compose.desktop {
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
             packageName = "shaker"
-            packageVersion = "1.0.0"
+            packageVersion = System.getenv("VERSION") ?: "1.0.0"
         }
     }
 }
